9437d5e44a78201752e0bb26c3f1c3cf4b71b3f4
gvenvivar <gven.inc@gmail.com>
2015-04-06 17:33:11 +0300

    test2

---
+++ mainJS.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('li#photo ul').hide();
+        $('li#photo').hover(
+        function() {$('li#photo ul').fadeIn('slow').stop(true, true)},
+        function() {$('li#photo ul').fadeOut().stop(true, true)});
+    $('.box').animate({left: '+=50px'}, 1000);
+}); 
+$(document).ready(function() {
+        $('.viewgallery').mouseenter(function(e) {
+            $(this).children('a').children('img').animate({ height: '299', left: '0', top: '0', width: '450'}, 100).stop(true, true);
+            $(this).children('a').children('span').fadeIn(200).stop(true, true);
+        }).mouseleave(function(e) {
+            $(this).children('a').children('img').animate({ height: '332', left: '-20', top: '-20', width: '500'}, 100);
+            $(this).children('a').children('span').fadeOut(200).stop(true, true);
+        });
+    });
+
+var imgs = ["img/slider/1.jpg","img/slider/2.jpg", "img/slider/3.jpg", "img/slider/4.jpg", "img/slider/5.jpg"];
+var n=0;
+var time=800;
+play=setInterval("chgImg(0)", 5000);
+
+function chgImg(number) {
+if (number!=0) n=number-2;
+ $('#slide_show').fadeOut(time, function() {    //для картинок
+  $(this).attr('src', imgs[n]).fadeIn(time);
+ });
+ n++;
+if (n>=imgs.length) n=0;
+};
+    
