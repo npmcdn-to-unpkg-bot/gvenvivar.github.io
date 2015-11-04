11869f66fe54783b700face6311f2643117daed5
gvenvivar <gven.inc@gmail.com>
2015-11-04 17:48:12 +0200

    new slider

--- js/common.js
+++ js/common.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+	$(".slider").owlCarousel({
+		items: 1,
+		animateOut: true,
+		loop: true,
+		autoplay: true,
+		autoplayHoverPause: true,
+		animateOut: 'fadeOut'
+	});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
@@ -44,19 +53,3 @@ $(document).ready(function() {
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
 });
-
-
-var imgs = ["img/slider/1.jpg","img/slider/2.jpg", "img/slider/3.jpg", "img/slider/4.jpg", "img/slider/5.jpg"];
-var n=0;
-var time=800;
-play=setInterval("chgImg(0)", 5000);
-
-function chgImg(number) {
-if (number!=0) n=number-2;
- $('#slide_show').fadeOut(time, function() {    //для картинок
-  $(this).attr('src', imgs[n]).fadeIn(time);
- });
- n++;
-if (n>=imgs.length) n=0;
-};
-    
